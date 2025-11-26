234458fe2559eb4502f495fd4e53be68e4097074
konef <k.one.for.all.k@gmail.com>
2025-11-27 00:01:56 +0900

    App.vue の更新

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
-  // 20251126_角井　編集したよ
+// 20251126_角井　編集したよ
+//VScodeで修正したよ
 </script>
 
 <template>
   <h1>You did it!</h1>
   <p>
-    
+
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
