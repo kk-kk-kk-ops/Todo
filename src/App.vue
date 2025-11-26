cf41ecbd970018aae9fb1b82b4f0a2a4cf7f12be
kk-kk-kk-ops <k.one.for.all.k@gmail.com>
2025-11-26 23:46:21 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
-<script setup></script>
+<script setup>
+  // 20251126_角井　編集したよ
+</script>
 
 <template>
   <h1>You did it!</h1>
   <p>
+    
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
